aa86f036492ae5ee978356ade5ba669aa39e505e
jarolthecoder <jarolr.webdev@gmail.com>
2025-02-20 21:52:43 -0600

    Adjusted resposinve team image

--- src/pages/about/components/TeamMembers/TeamMembers.jsx
+++ src/pages/about/components/TeamMembers/TeamMembers.jsx
@@ -2,18 +2,27 @@ import { useMediaQuery } from "@/hooks";
 import { breakpoints } from "@/lib";
 import { BackgroundPattern } from "@/components/ui";
 import styles from "./TeamMembers.module.css";
-import teamImg from "@/assets/about/mobile/image-team-members.jpg";
+import teamImgDesktop from "@/assets/about/desktop/image-team-members.jpg";
+import teamImgTablet from "@/assets/about/tablet/image-team-members.jpg";
+import teamImgMobile from "@/assets/about/mobile/image-team-members.jpg";
 
 export const TeamMembers = () => {
   const matchesMobile = useMediaQuery(breakpoints.sm);
+  const matchesTablet = useMediaQuery(breakpoints.md);
 
   return (
     <section className={styles.main}>
-      {matchesMobile ? (
-        <img src={teamImg} alt="PayApi Team working together at a cafe" />
-      ) : (
-        <div className={styles.teamImgHolder}></div>
-      )}
+      <img
+        src={
+          matchesMobile
+            ? teamImgMobile
+            : matchesTablet
+            ? teamImgTablet
+            : teamImgDesktop
+        }
+        alt="PayApi Team working together at a cafe"
+        width="100%"
+      />
       <BackgroundPattern className={styles.backgoundPatternImg} />
     </section>
   );
